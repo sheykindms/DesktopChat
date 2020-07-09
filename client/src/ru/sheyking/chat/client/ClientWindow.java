f3d5e8054e9d0fbd3e3aaa0648db3da3a2ee189f
sheykindms <sheykindms@gmail.com>
2020-07-09 12:22:12 +0300

    Initial commit

---
+++ client/src/ru/sheyking/chat/client/ClientWindow.java
@@ -0,0 +1,8 @@
+package ru.sheyking.chat.client;
+
+public class ClientWindow {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ server/src/ru/sheyking/chat/server/ChatServer.java
@@ -0,0 +1,8 @@
+package ru.sheyking.chat.server;
+
+public class ChatServer {
+
+    public static void main(String[] args) {
+
+    }
+}
